d0ae128c75e6bfd60a20f25d43ba548900288b39
Martijn Laarman <Mpdreamz@gmail.com>
2024-10-03 16:34:06 +0200

    Start on infra for github actions (#3)
    * Start on infra for github actions
    
    * build
    
    * fix
    
    * Test for licences in PR
    
    * add publish target

--- build/Program.cs
+++ build/Program.cs
@@ -1,7 +1,31 @@
 using ConsoleAppFramework;
 using ProcNet;
+using Zx;
+//using static Zx.Env;
 
-// ReSharper disable RedundantLambdaParameterType
+var app = ConsoleApp.Create();
+app.Add("", async (Cancel _) =>
+{
+	await "dotnet tool restore";
+	await "dotnet build -c Release --verbosity minimal";
+});
+app.Add("validate-licenses", (Cancel _) =>
+{
+	string[] args =
+	[
+		"-u", "-t", "-i", "docs-builder.sln", "--use-project-assets-json",
+		"--forbidden-license-types", "build/forbidden-license-types.json",
+		"--packages-filter", "#System..*#"
+	];
+	Proc.Exec("dotnet", ["dotnet-project-licenses", ..args]);
+});
 
-ConsoleApp.Run(args, () => Proc.Exec("dotnet", "--help"));
-
+app.Add("publish", async (Cancel _) =>
+{
+	var source = "src/docs-builder/docs-builder.csproj";
+	await $"""
+		dotnet publish {source} -c Release -o .artifacts/publish \
+			--self-contained true /p:PublishTrimmed=true /p:PublishSingleFile=false /p:PublishAot=true
+		""";
+});
+await app.RunAsync(args);
