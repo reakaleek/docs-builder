667b6d6ac68f0ba78198c6ae67e5149ba8278c8f
Martijn Laarman <Mpdreamz@gmail.com>
2024-11-13 15:20:33 +0100

    Add validation to version directives (#65)

--- src/Elastic.Markdown/Myst/Directives/VersionBlock.cs
+++ src/Elastic.Markdown/Myst/Directives/VersionBlock.cs
@@ -1,6 +1,9 @@
 // Licensed to Elasticsearch B.V under one or more agreements.
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
+
+using Elastic.Markdown.Helpers;
+
 namespace Elastic.Markdown.Myst.Directives;
 
 public class VersionBlock(DirectiveBlockParser parser, string directive, Dictionary<string, string> properties)
@@ -8,20 +11,30 @@ public class VersionBlock(DirectiveBlockParser parser, string directive, Diction
 {
 	public override string Directive => directive;
 	public string Class => directive.Replace("version", "");
+	public SemVersion? Version { get; private set; }
 
-	public string Title
-	{
-		get
-		{
-			var title = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(directive.Replace("version", "version "));
-			if (!string.IsNullOrEmpty(Arguments))
-				title += $" {Arguments}";
-
-			return title;
-		}
-	}
+	public string Title { get; private set; } = string.Empty;
 
 	public override void FinalizeAndValidate(ParserContext context)
 	{
+		var tokens = Arguments?.Split(" ", 2, StringSplitOptions.RemoveEmptyEntries) ?? [];
+		if (tokens.Length < 1)
+		{
+			EmitError(context, $"{directive} needs exactly 2 arguments: <version> <title>");
+			return;
+		}
+
+		if (!SemVersion.TryParse(tokens[0], out var version))
+		{
+			EmitError(context, $"{tokens[0]} is not a valid version");
+			return;
+		}
+
+		Version = version;
+		var title = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(directive.Replace("version", "version "));
+		title += $" ({Version})";
+		if (tokens.Length > 1 && !string.IsNullOrWhiteSpace(tokens[1]))
+			title += $": {tokens[1]}";
+		Title = title;
 	}
 }
--- tests/Elastic.Markdown.Tests/Directives/VersionTests.cs
+++ tests/Elastic.Markdown.Tests/Directives/VersionTests.cs
@@ -1,6 +1,8 @@
 // Licensed to Elasticsearch B.V under one or more agreements.
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
+
+using Elastic.Markdown.Helpers;
 using Elastic.Markdown.Myst.Directives;
 using FluentAssertions;
 using Markdig.Syntax;
@@ -10,7 +12,7 @@ namespace Elastic.Markdown.Tests.Directives;
 
 public abstract class VersionTests(ITestOutputHelper output, string directive) : DirectiveTest<VersionBlock>(output,
 $$"""
-```{{{directive}}}
+```{{{directive}}} 1.0.1-beta1 more information
 Version brief summary
 ```
 A regular paragraph.
@@ -22,26 +24,29 @@ A regular paragraph.
 
 	[Fact]
 	public void SetsCorrectDirectiveType() => Block!.Directive.Should().Be(directive);
+
+	[Fact]
+	public void SetsVersion() => Block!.Version.Should().Be(new SemVersion(1, 0, 1, "beta1"));
 }
 
 public class VersionAddedTests(ITestOutputHelper output) : VersionTests(output, "versionadded")
 {
 	[Fact]
-	public void SetsTitle() => Block!.Title.Should().Be("Version Added");
+	public void SetsTitle() => Block!.Title.Should().Be("Version Added (1.0.1-beta1): more information");
 }
 
 public class VersionChangedTests(ITestOutputHelper output) : VersionTests(output, "versionchanged")
 {
 	[Fact]
-	public void SetsTitle() => Block!.Title.Should().Be("Version Changed");
+	public void SetsTitle() => Block!.Title.Should().Be("Version Changed (1.0.1-beta1): more information");
 }
 public class VersionRemovedTests(ITestOutputHelper output) : VersionTests(output, "versionremoved")
 {
 	[Fact]
-	public void SetsTitle() => Block!.Title.Should().Be("Version Removed");
+	public void SetsTitle() => Block!.Title.Should().Be("Version Removed (1.0.1-beta1): more information");
 }
 public class VersionDeprectatedTests(ITestOutputHelper output) : VersionTests(output, "deprecated")
 {
 	[Fact]
-	public void SetsTitle() => Block!.Title.Should().Be("Deprecated");
+	public void SetsTitle() => Block!.Title.Should().Be("Deprecated (1.0.1-beta1): more information");
 }
