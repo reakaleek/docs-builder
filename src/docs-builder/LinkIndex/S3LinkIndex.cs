72ddc34d36d6e9a7e3df10f073642fbed986c178
Jan Calanog <jan.calanog@elastic.co>
2025-01-13 16:32:24 +0100

    Add license header

--- src/docs-builder/LinkIndex/S3LinkIndex.cs
+++ src/docs-builder/LinkIndex/S3LinkIndex.cs
@@ -1,7 +1,9 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
 using Amazon.S3;
 using Amazon.S3.Transfer;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 
 namespace Documentation.Builder.LinkIndex;
 
