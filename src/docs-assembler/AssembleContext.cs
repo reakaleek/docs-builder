33129e78c9be80d29865cccbbb9a3d1e38392cca
Martijn Laarman <Mpdreamz@gmail.com>
2025-03-11 12:48:33 +0200

    Embedd assembler.yml in binary temporarily before its homed in docs-content (#716)

--- src/docs-assembler/AssembleContext.cs
+++ src/docs-assembler/AssembleContext.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information
 
 using System.IO.Abstractions;
+using System.Reflection;
 using Documentation.Assembler.Configuration;
 using Elastic.Markdown.Diagnostics;
 using Elastic.Markdown.IO;
@@ -29,8 +30,32 @@ public class AssembleContext
 		WriteFileSystem = writeFileSystem;
 
 		var configPath = Path.Combine(Paths.Root.FullName, "src/docs-assembler/assembler.yml");
+		// temporarily fallback to embedded assembler.yml
+		// This will live in docs-content soon
+		if (!ReadFileSystem.File.Exists(configPath))
+			ExtractAssemblerConfiguration(configPath);
+
 		ConfigurationPath = ReadFileSystem.FileInfo.New(configPath);
 		Configuration = AssemblyConfiguration.Deserialize(ReadFileSystem.File.ReadAllText(ConfigurationPath.FullName));
 		OutputDirectory = ReadFileSystem.DirectoryInfo.New(output ?? ".artifacts/assembly");
+	}
+
+	private void ExtractAssemblerConfiguration(string configPath)
+	{
+		var embeddedStaticFiles = Assembly.GetExecutingAssembly()
+			.GetManifestResourceNames()
+			.ToList();
+		var configFile = embeddedStaticFiles.First(f => f.EndsWith("assembler.yml"));
+		using var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(configFile);
+		if (resourceStream == null)
+			return;
+
+		var outputFile = WriteFileSystem.FileInfo.New(configPath);
+		if (outputFile.Directory is { Exists: false })
+			outputFile.Directory.Create();
+		using var stream = outputFile.OpenWrite();
+		resourceStream.CopyTo(stream);
+
+
 	}
 }
