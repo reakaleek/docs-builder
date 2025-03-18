99b12f8bf7a82107ffcf59dacd199d00a965e9db
Felipe Cotti <felipe.cotti@elastic.co>
2025-03-18 11:06:36 -0300

    Preload fonts in use as per CSS definitions (#765)
    * Preload fonts in use as per CSS definitions
    
    * Adjust FontUriCache typing and stricten Regex
    
    * Readjust link header ordering.
    
    * Fix accessor to private.

---
+++ src/Elastic.Markdown/Helpers/Preloader.cs
@@ -0,0 +1,39 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Elastic.Markdown.Helpers;
+
+public static partial class FontPreloader
+{
+	private static IReadOnlyCollection<string>? FontUriCache = null!;
+
+	public static async Task<IReadOnlyCollection<string>> GetFontUrisAsync() => FontUriCache ??= await LoadFontUrisAsync();
+	private static async Task<IReadOnlyCollection<string>> LoadFontUrisAsync()
+	{
+		var cachedFontUris = new List<string>();
+		var assembly = Assembly.GetExecutingAssembly();
+		var stylesResourceName = assembly.GetManifestResourceNames().First(n => n.EndsWith("styles.css"));
+
+		using var cssFileStream = new StreamReader(assembly.GetManifestResourceStream(stylesResourceName)!);
+
+		var cssFile = await cssFileStream.ReadToEndAsync();
+		var matches = FontUriRegex().Matches(cssFile);
+
+		foreach (Match match in matches)
+		{
+			if (match.Success)
+				cachedFontUris.Add($"/_static/{match.Groups[1].Value}");
+		}
+		FontUriCache = cachedFontUris;
+		return FontUriCache;
+	}
+
+	[GeneratedRegex(@"url\([""']?([^""'\)]+?\.(woff2|ttf|otf))[""']?\)", RegexOptions.Multiline | RegexOptions.Compiled)]
+	private static partial Regex FontUriRegex();
+}
